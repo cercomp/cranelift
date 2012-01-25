b1df623bf7f59ea0531d922cb033cd4054b5bc17
Danilo Resende <danilo.isr@gmail.com>
2012-01-25 12:07:43 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cranelift::Application.config.secret_token = 'ea32559b055af74fe5e946419f0913c31af1a9fecdca9c3248ab965ccf96560c7ee53f4164c28a5336d4e55b8cfebb653c4f63bedbaace19bffa6a3571875b39'
