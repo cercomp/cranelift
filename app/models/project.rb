6e677414090f0e442c08341a5ac1934a47c08e23
Danilo Resende <danilo.isr@gmail.com>
2012-02-07 12:13:42 -0200

    added svn methods to update and get logs

--- app/models/project.rb
+++ app/models/project.rb
@@ -14,4 +14,26 @@ class Project < ActiveRecord::Base
   validates :autoupdate_password,
             :presence => true,
             :if => "!enable_autoupdate.nil?"
+
+  # TODO usar uma classe abstrata para decidir qual scm usar
+  def checkout
+    svn.checkout(self.url, project_path)
+  end
+
+  def update (rev)
+    svn.update(project_path, rev)
+  end
+
+  def log (start = 0, limit = 0)
+    svn.log(project_path, start, 'HEAD', limit)
+  end
+
+  private
+  def project_path
+    File.join(Rails.root, 'projects', self.name)
+  end
+
+  def svn
+    @svn ||= CraneLift::SvnScm.new
+  end
 end
--- lib/cranelift/crane_lift.rb
+++ lib/cranelift/crane_lift.rb
@@ -3,14 +3,26 @@ require 'svn/client'
 module CraneLift
 
   class SvnScm
-    @@ctx = Svn::Client::Context.new
+    def initialize
+      @ctx = Svn::Client::Context.new
+      @ctx.add_simple_provider
+      @ctx.add_ssl_server_trust_file_provider
+    end
 
-    class << self
-      def checkout(url, destination)
-        @@ctx.add_simple_provider
-        @@ctx.add_ssl_server_trust_file_provider
-        @@ctx.checkout(url, destination)
+    def checkout(url, destination)
+      @ctx.checkout(url, destination)
+    end
+
+    def update(path, rev)
+      @ctx.update(path, rev)
+    end
+
+    def log(paths, start_rev, end_rev, limit)
+      logs = []
+      @ctx.log(paths, start_rev, end_rev, limit, true, nil) do |changed_paths, rev, author, date, message|
+        logs << [changed_paths, rev, author, date, message]
       end
+      logs
     end
 
   end
