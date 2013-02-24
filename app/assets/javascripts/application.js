b8c20eaf942df0c1dc5c9e84aedd901391c0b763
Danilo Resende <danilo.isr@gmail.com>
2013-02-24 17:28:44 -0300

    Update chosen and maskedinput

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require libs/chosen/chosen.jquery.min
-//= require libs/maskedinput/jquery.maskedinput-1.3.js
+//= require libs/chosen/chosen.jquery
+//= require libs/maskedinput/jquery.maskedinput
 //= require general
 //= require_tree ./admin/
